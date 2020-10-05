456e7c09320989e2f368ed197c9fcb9d6ad05ff0
Ashok443 <kondapurashok@gmail.com>
2020-10-05 12:04:40 +0530

    Enhanced model object

---
+++ profile-service/src/main/java/com/example/demo/model/TotalExperiance.java
@@ -0,0 +1,28 @@
+package com.example.demo.model;
+
+public class TotalExperiance {
+
+	private int inYears;
+	private int inMonths;
+	
+	
+	public TotalExperiance(int inYears, int inMonths) {
+		super();
+		this.inYears = inYears;
+		this.inMonths = inMonths;
+	}
+	public int getInYears() {
+		return inYears;
+	}
+	public void setInYears(int inYears) {
+		this.inYears = inYears;
+	}
+	public int getInMonths() {
+		return inMonths;
+	}
+	public void setInMonths(int inMonths) {
+		this.inMonths = inMonths;
+	}
+	
+	
+}
